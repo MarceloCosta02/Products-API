a7c03ded799fb02a36869e59dd758507dbc85f11
Marcelo Ponte <marceloforce4@gmail.com>
2020-06-24 19:29:54 -0300

    Versão inicial estável

---
+++ ProductAPIWithEF/ProductAPIWithEF/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductAPIWithEF.Models;
+
+namespace ProductAPIWithEF.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+             
+    }
+}
